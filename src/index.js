fa2ac023a0ff76614525437c8bc3d7289b606310
Vilmarys R <43301644+Vilmango@users.noreply.github.com>
2019-10-19 23:28:54 -0500

    Updated index.js
    Shorted the style changes for the buttons into variables to avoid repetition on click events.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-const btn1 = document.getElementById('boton1');
-btn1.addEventListener('click', () => {
-  document.getElementById('root').style.display = 'none';
-  document.getElementById('secondScreenCod').style.display = 'block';
-  document.getElementById('fourthScreenReturn').style.display = 'block';
-  document.getElementById('header').style.display = 'block';  
-  
+const root1 = document.getElementById('root');
+const secondSC = document.getElementById('secondScreenCod');
+const thirdSDC = document.getElementById('thirdScreenDecod');
+const fourthSR = document.getElementById('fourthScreenReturn');
+const head = document.getElementById('header');
+
+const btn1C = document.getElementById('boton1C');
+btn1C.addEventListener('click', () => {
+  root1.style.display = 'none';
+  secondSC.style.display = 'block';
+  fourthSR.style.display = 'block';
+  header.style.display = 'block';  
 });
-const btn2 = document.getElementById('boton2');
-btn2.addEventListener('click', () => {
-  document.getElementById('root').style.display = 'none';
-  document.getElementById('secondScreenCod').style.display = 'none';
-  document.getElementById('thirdScreenDecod').style.display = 'block';
-  document.getElementById('fourthScreenReturn').style.display = 'block';
-  document.getElementById('header').style.display = 'block';
+const btn2D = document.getElementById('boton2D');
+btn2D.addEventListener('click', () => {
+  root1.style.display = 'none';
+  secondSC.style.display = 'none';
+  thirdSDC.style.display = 'block';
+  fourthSR.style.display = 'block';
+  head.style.display = 'block';
 });
-const btn3 = document.getElementById('botonRegreso');
-btn3.addEventListener('click', () => {
-  document.getElementById('root').style.display = 'block';
-  document.getElementById('secondScreenCod').style.display = 'none';
-  document.getElementById('thirdScreenDecod').style.display = 'none';
-  document.getElementById('fourthScreenReturn').style.display = 'none';
-  document.getElementById('header').style.display = 'none';
+const btn3Reg = document.getElementById('botonRegreso');
+btn3Reg.addEventListener('click', () => {
+  root1.style.display = 'block';
+  secondSC.style.display = 'none';
+  thirdSDC.style.display = 'none';
+  fourthSR.style.display = 'none';
+  head.style.display = 'none';
   document.getElementById('textCod').value = '';
   document.getElementById('textDecod').value = '';
   document.getElementById('complex1').value = '';
@@ -28,21 +33,17 @@ btn3.addEventListener('click', () => {
   document.getElementById('resultadoCod').innerHTML='';
   document.getElementById('resultadoDecod').innerHTML='';
 });
-const btn4 = document.getElementById('botonCodificar');
-btn4.addEventListener('click', () => {
+const btn4Cod = document.getElementById('botonCodificar');
+btn4Cod.addEventListener('click', () => {
   const texttoCod = document.getElementById('textCod').value;
   const complexity1 = document.getElementById('complex1').value;
-  // console.log(texttoCod);
-  // console.log(complexity1);
   const CResult = window.cipher.encode(complexity1, texttoCod);
   document.getElementById('resultadoCod').innerHTML = CResult;
 });
-const btn5 = document.getElementById('botonDecodificar');
-btn5.addEventListener('click', () => {
+const btn5Decod = document.getElementById('botonDecodificar');
+btn5Decod.addEventListener('click', () => {
   const textDecod = document.getElementById('textDecod').value;
   const complexity2 = document.getElementById('complex2').value;
-  // console.log(textDecod);
-  // console.log(complexity2);
   const DResult = window.cipher.decode(complexity2, textDecod);
   document.getElementById('resultadoDecod').innerHTML = DResult;
 });
