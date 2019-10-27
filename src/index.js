67866a9134b5f192242a5fc3cae5af46dac447f0
Vilmarys R <43301644+Vilmango@users.noreply.github.com>
2019-10-27 15:02:56 -0500

    Update index.js
    Updated event button function accordingly to new css screen transition

--- src/index.js
+++ src/index.js
@@ -1,49 +1,54 @@
+const next = document.getElementById("nextlink");
+const previous = document.getElementById("previouslink");
+const last =document.getElementById("thirdlink")
+const container = document.getElementById("container");
 const root1 = document.getElementById('root');
-const secondSC = document.getElementById('secondScreenCod');
-const thirdSDC = document.getElementById('thirdScreenDecod');
-const fourthSR = document.getElementById('fourthScreenReturn');
-const head = document.getElementById('header');
-
 const btn1C = document.getElementById('boton1C');
-btn1C.addEventListener('click', () => {
-  root1.style.display = 'none';
-  secondSC.style.display = 'block';
-  fourthSR.style.display = 'block';
-  head.style.display = 'block';  
-});
 const btn2D = document.getElementById('boton2D');
-btn2D.addEventListener('click', () => {
-  root1.style.display = 'none';
-  secondSC.style.display = 'none';
-  thirdSDC.style.display = 'block';
-  fourthSR.style.display = 'block';
-  head.style.display = 'block';
-});
 const btn3Reg = document.getElementById('botonRegreso');
+const btn4Reg = document.getElementById('botonRegreso2');
+const btn4Cod = document.getElementById('botonCodificar');
+const btn5Decod = document.getElementById('botonDecodificar');
+btn1C.addEventListener('click', () => {
+    container.classList.add("nextlink");
+});
+
+btn2D.addEventListener('click', () => {
+  container.classList.add("secondlink");
+});
+
 btn3Reg.addEventListener('click', () => {
-  root1.style.display = 'block';
-  secondSC.style.display = 'none';
-  thirdSDC.style.display = 'none';
-  fourthSR.style.display = 'none';
-  head.style.display = 'none';
   document.getElementById('textCod').value = '';
   document.getElementById('textDecod').value = '';
   document.getElementById('complex1').value = '';
   document.getElementById('complex2').value = '';
   document.getElementById('resultadoCod').innerHTML='';
   document.getElementById('resultadoDecod').innerHTML='';
+  container.classList.remove("nextlink");
 });
-const btn4Cod = document.getElementById('botonCodificar');
+
+btn4Reg.addEventListener('click', () => {
+  document.getElementById('textCod').value = '';
+  document.getElementById('textDecod').value = '';
+  document.getElementById('complex1').value = '';
+  document.getElementById('complex2').value = '';
+  document.getElementById('resultadoCod').innerHTML='';
+  document.getElementById('resultadoDecod').innerHTML='';
+  container.classList.remove("secondlink");
+});
+
 btn4Cod.addEventListener('click', () => {
   const texttoCod = document.getElementById('textCod').value;
   const complexity1 = document.getElementById('complex1').value;
   const CResult = window.cipher.encode(complexity1, texttoCod);
   document.getElementById('resultadoCod').innerHTML = CResult;
+  container.classList.remove();
 });
-const btn5Decod = document.getElementById('botonDecodificar');
+
 btn5Decod.addEventListener('click', () => {
   const textDecod = document.getElementById('textDecod').value;
   const complexity2 = document.getElementById('complex2').value;
   const DResult = window.cipher.decode(complexity2, textDecod);
   document.getElementById('resultadoDecod').innerHTML = DResult;
+  container.classList.remove();
 });
